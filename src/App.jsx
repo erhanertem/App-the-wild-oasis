ea097b24086ce0ce19880597b100e66724994a22
Erhan Ertem <eertem@gmail.com>
2024-02-22 20:59:59 +0300

    Increase reusability of styled components - defaultProps

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import GlobalStyles from './styles/GlobalStyles';
 import { Button } from './ui/Button';
 import { Input } from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 //STYLES KEPT ALONG WITH THE COMPONENT - THEY DO NOT GET USED ELSEWHERE!!!
 // const H1 = styled.h1`
@@ -14,7 +15,7 @@ import Heading from './ui/Heading';
 // `;
 
 const StyledApp = styled.div`
-	background-color: orangered;
+	/* background-color: orangered; */
 	padding: 20px;
 `;
 
@@ -23,17 +24,31 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading as="h1">THE WILD OASIS</Heading>
-				{/* type prop only reflects the custom css change proposed */}
-
-				<Heading as="h2">Check in and out</Heading>
-				<Button onClick={() => alert('Checked In')}>Check in</Button>
-				<Button onClick={() => alert('Checked Out')}>Check out</Button>
-
-				<Heading as="h3">Form</Heading>
-				{/* as prop actually reflects both the custom css change and also the element */}
-				<Input type="number" placeholder="Number of guests" />
-				<Input type="number" placeholder="Number of guests" />
+				{/* <Row type='vertical'> */}
+				{/* We dont need to disclose vertical as type prop as its given as defaultProp */}
+				<Row>
+					<Row type="horizontal">
+						<Heading as="h1">THE WILD OASIS</Heading>
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							{/* <Button variation="primary" size="medium" onClick={() => alert('Checked In')}> */}
+							{/* We dont even need to disclose variation and size props as they are defaultProps */}
+							<Button onClick={() => alert('Checked In')}>Check in</Button>
+							<Button variation="secondary" size="small" onClick={() => alert('Checked Out')}>
+								Check out
+							</Button>
+						</div>
+					</Row>
+					<Row>
+						<Heading as="h3">Form</Heading>
+						<form>
+							{/* type prop only reflects the custom css change proposed */}
+							{/* as prop actually reflects both the custom css change and also the element */}
+							<Input type="number" placeholder="Number of guests" />
+							<Input type="number" placeholder="Number of guests" />
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
