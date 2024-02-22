87d69b702ca75da4a356f8b6e2368182104ba7da
Erhan Ertem <eertem@gmail.com>
2024-02-22 20:24:09 +0300

    Importing styled components, using css literal function, & prefix, as  and type props

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
-const H1 = styled.h1`
-	font-size: 30px;
-	font-weight: 600;
-	background-color: yellow;
-`;
-const Button = styled.button`
-	font-size: 1.4rem;
-	padding: 1.2rem 1.6rem;
-	font-weight: 500;
-	border: none;
-	border-radius: 7px;
-	background-color: purple;
-	color: white;
-	cursor: pointer;
+//STYLES KEPT OUTSIDE FOR SHARING W/MULTIPLE COMPONENTS
+import { Button } from './ui/Button';
+import { Input } from './ui/Input';
+import Heading from './ui/Heading';
 
-	margin: 20px;
-`;
-
-const Input = styled.input`
-	border: 1px solid #ddd;
-	border-radius: 5px;
-	padding: 0.8rem 1.2rem;
-`;
+//STYLES KEPT ALONG WITH THE COMPONENT - THEY DO NOT GET USED ELSEWHERE!!!
+// const H1 = styled.h1`
+// 	font-size: 30px;
+// 	font-weight: 600;
+// 	background-color: yellow;
+// `;
 
 const StyledApp = styled.div`
 	background-color: orangered;
@@ -35,10 +23,16 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<H1>THE WILD OASIS</H1>
+				<Heading as="h1">THE WILD OASIS</Heading>
+				{/* type prop only reflects the custom css change proposed */}
+
+				<Heading as="h2">Check in and out</Heading>
 				<Button onClick={() => alert('Checked In')}>Check in</Button>
 				<Button onClick={() => alert('Checked Out')}>Check out</Button>
 
+				<Heading as="h3">Form</Heading>
+				{/* as prop actually reflects both the custom css change and also the element */}
+				<Input type="number" placeholder="Number of guests" />
 				<Input type="number" placeholder="Number of guests" />
 			</StyledApp>
 		</>
