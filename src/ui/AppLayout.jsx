e71c43c79c36ef5b1d9c35710442cb4882cf83df
Erhan Ertem <eertem@gmail.com>
2024-02-28 14:48:22 +0300

    Provide styled container for child routes (Outlet) to limit its size

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,34 +1,44 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import Sidebar from './Sidebar';
-import Header from './Header';
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
-	display: grid;
-	grid-template-columns: 26rem 1fr;
-	grid-template-rows: auto 1fr;
-	height: 100vh;
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
 `;
 
 const Main = styled.main`
-	background-color: var(--color-grey-50);
-	padding: 4rem 4.8rem 6.4rem;
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
-	return (
-		<StyledAppLayout>
-			<Header />
-			<Sidebar />
-			<Main>
-				{/* Outlet help us use children routes inside this AppLayout route */}
-				{/* Whatever comes from the pages remains inside the main */}
-				<Outlet />
-			</Main>
-		</StyledAppLayout>
-	);
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Container>
+          {/* Outlet help us use children routes inside this AppLayout route */}
+          {/* Whatever comes from the pages remains inside the main */}
+          <Outlet />
+        </Container>
+      </Main>
+    </StyledAppLayout>
+  );
 }
 
 export default AppLayout;
