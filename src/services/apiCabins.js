410312e7d0f3726219b1144fc8000304d69f2de2
Erhan Ertem <eertem@gmail.com>
2024-02-28 17:22:47 +0300

    Add Create ability to cabin API

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,25 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  let { data, error } = await supabase.from("cabins").select("*");
+  const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
   }
 
   return data;
