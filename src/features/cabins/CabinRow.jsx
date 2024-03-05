72a2c8adbd5465c3e93e35e4fb6e18a5a58d320c
Erhan Ertem <eertem@gmail.com>
2024-03-05 18:35:46 +0300

    Conditionalize the discount info

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -105,7 +105,11 @@ function CabinRow({
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity} guests</div>
         <Price>{formatCurrency(regularPrice)}</Price>
-        <Discount>{formatCurrency(discount)}</Discount>
+        {discount ? (
+          <Discount>{formatCurrency(discount)}</Discount>
+        ) : (
+          <span>&mdash;</span>
+        )}
         <div>
           <button
             onClick={() => {
