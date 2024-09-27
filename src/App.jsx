e3301af708405ffd37cba917c4465ad2e9bfa963
Erhan Ertem <eertem@gmail.com>
2024-09-27 14:41:36 +0300

    Experiment w/ more re-usable type propped components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
-// > CHILD JSX ELEMENT STYLING
-// STYLED-COMPONENTS MAKE USE OF ES6'S TAGGED TEMPLATE LITERALS FEATURE
-// > PARENT JSX ELEMENT STYLING
-// NOTE: THE PARENT JSX GETS STYLEDAPP COMPONENT NAME BY CONVENTION IF THIS HAS TO BE STYLED AS WELL.
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -18,21 +15,32 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-        <Heading as="h2">Checkin | out</Heading>
-        <Button onClick={() => alert('Checked in')}>Check-in</Button>
-        <Button onClick={() => alert('Checked out')}>Check-out</Button>
+            <div>
+              <Heading as="h2">Checkin | out</Heading>
+              <Button onClick={() => alert('Checked in')}>Check-in</Button>
+              <Button onClick={() => alert('Checked out')}>Check-out</Button>
+            </div>
+          </Row>
 
-        <Heading as="h3">Form</Heading>
-        <Input
-          type="number"
-          placeholder="Number of Rooms"
-        ></Input>
-        <Input
-          type="number"
-          placeholder="Number of Guests"
-        ></Input>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+
+            <form>
+              <Input
+                type="number"
+                placeholder="Number of Rooms"
+              ></Input>
+              <Input
+                type="number"
+                placeholder="Number of Guests"
+              ></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
