e6d0b88b25dea628966703f4d459fd0c9a36e796
Erhan Ertem <eertem@gmail.com>
2024-09-27 11:49:35 +0300

    Keep non-reusable styled components inside the react component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 // > CHILD JSX ELEMENT STYLING
-// STYLED-COMPONENTS MAKE USE OF ES6'S TAGGED LITERALS FEATURE
-// CREATE REUSABLE REACT COMPONENTS W/STYLED-COMPONENTS CSS
+// STYLED-COMPONENTS MAKE USE OF ES6'S TAGGED TEMPLATE LITERALS FEATURE
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8 rem 1.2rem;
 `;
 
 // > PARENT JSX ELEMENT STYLING
