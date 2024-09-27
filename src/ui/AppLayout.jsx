f4cd126563d90f2713fc45601609250fb0a7fa6f
Erhan Ertem <eertem@gmail.com>
2024-09-27 23:23:25 +0300

    Setup applayout style sub components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,31 @@
 import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import styled from 'styled-components';
+
+// Parent styling
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100dvh;
+`;
+// Child styling
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
-      <p>APP LAYOUT</p>
-      <Outlet />
-    </div>
+    <StyledAppLayout>
+      <Sidebar />
+      <Header />
+      <Main>
+        {/* Let child routes pass thru RR Outlet component - AKA contents coming from pages */}
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
