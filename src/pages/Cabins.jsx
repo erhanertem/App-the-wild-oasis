92af29f6db100d288d9343b3aee356149767508e
Erhan Ertem <eertem@gmail.com>
2024-09-29 05:53:50 +0300

    Utilize the getCabins API call in the Cabins page @ initial mount

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,15 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import { getCabins } from '../services/apiCabins';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Cabins() {
+  useEffect(function () {
+    (async function () {
+      const data = await getCabins();
+      console.log(data);
+    })();
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
