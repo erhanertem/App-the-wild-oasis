082bb6ebfec9b8c5203592297d3706bb38c42126
Erhan Ertem <eertem@gmail.com>
2024-09-30 10:27:09 +0300

    Register entered data to react hook form via RHF useForm/register hook for all input fields

--- README.md
+++ README.md
@@ -20,12 +20,13 @@ Key take-aways from this project:
   - Dynamic styled-components
     - template literals + conditional statements
   - Creating re-usable styled components
-    - Propping via custom_named prop
-    - Propping via as prop (HTML tag modifier)
+    - Propping via 'custom_named' prop
+    - Propping via 'as' prop (HTML tag modifier)
     - defaultProps function - Set defaults for the propping type
   - Create styled componenet contemplating a (ReactRouter) component
     - styled() SC function
 - React Router
+
   - Declariative route setup
     - BrowserRouter RR component
     - Routes RR component
@@ -35,13 +36,18 @@ Key take-aways from this project:
       - replace attribute
     - Create wrapper component (routeless) RR route used across multiple routes
       - \<Outlet/> RR component - Have wrapper RR route pass thru child routes
+
 - Tanstack Query
+
   - Create TQ client
   - Provide TQ client via \<QueryClientProvider/>
   - Query Remote Data
     - 'GET' data w/ useQuery TQ hook
     - 'DELETE' data w/ useMutation TQ hook
       - Get instance of TQ client via useQueryClient TQ hook
+
+- Reach-Hook-Form
+  - useForm() hook
 
 &emsp;
 
@@ -53,7 +59,8 @@ Key take-aways from this project:
 
 ![JS](https://img.shields.io/badge/JavaScript-323330?style=square&logo=javascript&logoColor=F7DF1E)
 ![React](https://img.shields.io/badge/React-20232A?style=square&logo=react&logoColor=61DAF)
+![Styled Components](https://img.shields.io/badge/styled--components-DB7093?style=square&logo=styled-components&logoColor=white)
 ![React Router](https://img.shields.io/badge/React_Router-CA4245?style=square&logo=react-router&logoColor=white)
 ![React Query](https://img.shields.io/badge/React_Query-FF4154?style=square&logo=ReactQuery&logoColor=white)
-![Styled Components](https://img.shields.io/badge/styled--components-DB7093?style=square&logo=styled-components&logoColor=white)
+![React Hook Form](https://img.shields.io/badge/React%20Hook%20Form-%23EC5990.svg?style=square&logo=reacthookform&logoColor=white)
 ![Vite](https://img.shields.io/badge/Vite-B73BFE?style=square&logo=vite&logoColor=FFD62E)
--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,10 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import Input from "../../ui/Input";
-import Form from "../../ui/Form";
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Textarea from "../../ui/Textarea";
+import Input from '../../ui/Input';
+import Form from '../../ui/Form';
+import Button from '../../ui/Button';
+import FileInput from '../../ui/FileInput';
+import Textarea from '../../ui/Textarea';
+import { useForm } from 'react-hook-form';
 
 const FormRow = styled.div`
   display: grid;
@@ -43,41 +44,77 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
+  // > #1. REACT-HOOK-FORM
+  const { register, handleSubmit } = useForm();
+
   return (
     <Form>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
+        <Input
+          type="text"
+          id="name"
+          // > #2. REGISTER THE ENTERED DATA TO REACT HOOK FORM - Register refers to id 'name' - creates onBlur and onChnage props in this styled Input component
+          {...register('name')}
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
+        <Input
+          type="number"
+          id="maxCapacity"
+          // > #2. REGISTER THE ENTERED DATA TO REACT HOOK FORM - Register refers to id 'maxCapacity' - creates onBlur and onChnage props in this styled Input component
+          {...register('maxCapacity')}
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
+        <Input
+          type="number"
+          id="regularPrice"
+          // > #2. REGISTER THE ENTERED DATA TO REACT HOOK FORM - Register refers to id 'regularPrice' - creates onBlur and onChnage props in this styled Input component
+          {...register('regularPrice')}
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          // > #2. REGISTER THE ENTERED DATA TO REACT HOOK FORM - Register refers to id 'discount' - creates onBlur and onChnage props in this styled Input component
+          {...register('discount')}
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
+        <Textarea
+          type="number"
+          id="description"
+          // > #2. REGISTER THE ENTERED DATA TO REACT HOOK FORM - Register refers to id 'description' - creates onBlur and onChnage props in this styled Input component
+          {...register('description')}
+          defaultValue=""
+        />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+        >
           Cancel
         </Button>
         <Button>Edit cabin</Button>
