ad001b1c3975195f628b7f2450d6be4b5880e046
Erhan Ertem <eertem@gmail.com>
2024-10-05 13:28:46 +0300

    Create TQ custom hook for UPDATE settings

---
+++ src/features/settings/useEditSetting.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+import { updateSetting as updateSettingApi } from '../../services/apiSettings';
+
+export function useEditSetting() {
+  const queryClient = useQueryClient(); // Get reference to queryClient for invalidation purposes
+
+  const { mutate: updateSetting, isLoading: isUpdatingSetting } = useMutation({
+    mutationFn: updateSettingApi,
+    // same as (newSetting) => updateSettingApi(newSetting)
+    onSuccess: () => {
+      toast.success('Settings updated successfully!'); // Display success message
+      queryClient.invalidateQueries({ queryKey: 'settings' }); // Trigger re-cache upon successful mutation on 'settings' Key
+    },
+    onError: (err) => toast.error(err.message), // Display error message
+  });
+
+  return { isUpdatingSetting, updateSetting };
+}
