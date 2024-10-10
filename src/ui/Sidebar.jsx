adc1fed16425f7a611f197a97e039f303d7f49e7
Erhan Ertem <eertem@gmail.com>
2024-10-10 12:59:12 +0300

    Add DATA UPLOADER component for dev purposes

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
-import styled from 'styled-components';
-import Logo from './Logo';
-import MainNav from './MainNav';
+import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 // Parent styling
 const StyledSidebar = styled.aside`
@@ -19,6 +20,8 @@ function Sidebar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyledSidebar>
   );
 }
