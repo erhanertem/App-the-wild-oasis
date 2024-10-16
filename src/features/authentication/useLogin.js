2513e60b7f2684938596feaa47741458b3a34f30
Erhan Ertem <eertem@gmail.com>
2024-10-16 22:22:48 +0300

    Add notes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -12,7 +12,7 @@ export function useLogin() {
     mutationFn: loginApi,
     onSuccess: (userObj) => {
       // console.log(userObj); // userObj as arged in login mutate fn -> {user:***, session:***}
-      // Manually inject cache data to user query key
+      // Manually inject cache data to user query key - NOT REQUIRED - JUST EXPERIMENTATION - SHORTLIVED AND WILL BE OVERWRITTEN BY USEUSER CACHING ANYWAYS
       queryClient.setQueryData(["user"], userObj.user);
       navigate(
         "/dashboard",
