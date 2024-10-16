fda13d482f61bec581cfb04dd7035d3637112a8b
Erhan Ertem <eertem@gmail.com>
2024-10-17 02:41:50 +0300

    Create a user signup form  and implement RHF for signup form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,32 +1,92 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
-// Email regex: /\S+@\S+\.\S+/
-
 function SignupForm() {
+  const {
+    register, // Register form field values
+    formState, // Gets the errors from onErrorFn handler out of RHF to be used in practical UI error handling mediums such as toaster
+    getValues, // Reads the values in the form fields
+    handleSubmit, // Dials in actual form submit handler fn
+  } = useForm();
+  // GET THE ERRORS OUT OF RHF AND USE IT IN JSX BODY CONDITIONALLY
+  const { errors } = formState;
+
+  // Handle form submission if validations are passed
+  function onSubmitFn(data) {
+    console.log(data);
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmitFn)}>
+      <FormRow
+        label="Full name"
+        error={errors?.fullName?.message}
+      >
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullName", { required: "This field is required" })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow
+        label="Email address"
+        error={errors?.email?.message}
+      >
+        <Input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Please enter a valid email address",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow
+        label="Password (min 8 characters)"
+        error={errors?.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password needs a minimum of 8 characters",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow
+        label="Repeat password"
+        error={errors?.passwordConfirm?.message}
+      >
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "This field is required",
+            validate: (value) =>
+              value === getValues().password || "Passwords do not match",
+          })}
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+        >
           Cancel
         </Button>
         <Button>Create new user</Button>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,17 +1,17 @@
-import supabase, { supabaseUrl } from './supabase';
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   // IMPORTANT: USING A TRY-CATCH BLOCK DOES NOT HELP RETRIEVE SUPABASE ERROR AS ERROR IS DESIGNED TO BE PART OF THE SUPABASE RESPONSE
   const { data, error: cabinReadError } = await supabase
-    .from('cabins')
-    .select('*');
+    .from("cabins")
+    .select("*");
   // GUARD CLAUSE - HANDLE ERROR OBJECT FROM SUPABASE RESPONSE
   if (cabinReadError) {
     // console.log(error): For general debugging, when you want to see the full error object including its stack trace and any custom properties.
     // console.log(error.message): When you want to log only the error message, usually for user-friendly logs or minimal output.
     // console.error(error): When you want to explicitly log an error in a visually distinct way, making it clear that it's an error, for better visibility and emphasis in logs.
-    console.error('Error fetching cabins:', cabinReadError);
-    throw new Error('Cabins could not be loaded');
+    console.error("Error fetching cabins:", cabinReadError);
+    throw new Error("Cabins could not be loaded");
   }
   //  RETURN DATA IF THERE IS NO SUPABASE ERROR RESPONSE
   return data;
@@ -23,13 +23,13 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
 
   // >CHECK FOR EDIT DATA
   // formData is the raw data which may contain either an image property w/ image URL or an Image File - So we need to identify the nature of the received data for the image property
-  const hasImageURL = typeof formData.image === 'string' ? true : false;
+  const hasImageURL = typeof formData.image === "string" ? true : false;
 
   // >CHECK FOR EDIT & NEW DATA
   // Depending on the nature of image data received...Create a image name from the received data suited for supabase storing @ cabins table
   const imageName = hasImageURL
-    ? formData.image.split('/').pop()
-    : `${Math.random()}-${formData.image[0].name}`.replaceAll('/', '');
+    ? formData.image.split("/").pop()
+    : `${Math.random()}-${formData.image[0].name}`.replaceAll("/", "");
 
   // >CHECK FOR EDIT & NEW DATA
   // Depending on the nature of image data...Create the image path w/ the imagename if image posses a new file information
@@ -42,14 +42,14 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
   const cleanedFormData = hasImageURL
     ? formData
     : { ...formData, image: imagePath };
-  console.log('👉👉👉adjustedCabinData :', cleanedFormData);
+  // console.log('👉👉👉adjustedCabinData :', cleanedFormData);
 
   // >#1.CREATE/EDIT CABIN
   let cabinData, cabinError;
   // >#1.1.CREATE A CABIN - INSERT DATA
   if (!idForCabinEditing) {
     const { data, error } = await supabase
-      .from('cabins')
+      .from("cabins")
       // Override imageURL only to provided data for cabin table submission
       .insert([cleanedFormData])
       .select()
@@ -64,9 +64,9 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
     // > #1.2.1.EDIT A CABIN W/NO NEW CABIN PHOTO ATTACHED
     if (hasImageURL) {
       const { data, error } = await supabase
-        .from('cabins')
+        .from("cabins")
         .update(cleanedFormData)
-        .eq('id', idForCabinEditing);
+        .eq("id", idForCabinEditing);
 
       cabinData = data;
       cabinError = error;
@@ -74,9 +74,9 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
     // > #1.2.2.EDIT A CABIN W/ NEW CABIN PHOTO ATTACHED
     else {
       const { data, error } = await supabase
-        .from('cabins')
+        .from("cabins")
         .update(cleanedFormData)
-        .eq('id', idForCabinEditing);
+        .eq("id", idForCabinEditing);
 
       cabinData = data;
       cabinError = error;
@@ -86,11 +86,11 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
   // GUARD CLAUSE - HANDLE CREATE/EDIT CABIN ERROR OBJECT FROM SUPABASE RESPONSE
   if (cabinError) {
     console.error(
-      `Error ${!idForCabinEditing ? 'creating' : 'editing'} cabin:`,
+      `Error ${!idForCabinEditing ? "creating" : "editing"} cabin:`,
       cabinError
     );
     throw new Error(
-      `Cabin could not be ${!idForCabinEditing ? 'created' : 'edited'}`
+      `Cabin could not be ${!idForCabinEditing ? "created" : "edited"}`
     );
   }
 
@@ -98,9 +98,9 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
   // >#2.1.UPLOAD IMAGE FILE
   if (!hasImageURL) {
     const { error: storageError } = await supabase.storage
-      .from('cabin-images')
+      .from("cabin-images")
       .upload(imageName, formData.image[0], {
-        cacheControl: '3600',
+        cacheControl: "3600",
         upsert: false,
       });
 
@@ -109,16 +109,16 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
       // > #2.1.1 RESPOND TO ERROR WHEN CREATING CABIN
       if (!idForCabinEditing) {
         // DELETE THE UPLOADED CABIN DATA DUE TO FAILURE UPLOADING THE CABIN IMAGE FILE
-        await supabase.from('cabins').delete().eq('id', formData.id);
+        await supabase.from("cabins").delete().eq("id", formData.id);
         // HANDLE UPLOAD ERROR
-        console.error('Error uploading image file:', storageError);
-        throw new Error('Cabin image could not be uploaded');
+        console.error("Error uploading image file:", storageError);
+        throw new Error("Cabin image could not be uploaded");
       }
       // > #2.1.2 RESPOND TO ERROR WHEN UPDATING CABIN+PHOTO FILE
       if (idForCabinEditing) {
         // HANDLE UPLOAD ERROR
-        console.error('Error uploading image file:', storageError);
-        throw new Error('Cabin image could not be uploaded');
+        console.error("Error uploading image file:", storageError);
+        throw new Error("Cabin image could not be uploaded");
       }
     }
   }
@@ -128,40 +128,40 @@ export async function createOrEditCabin(formData, idForCabinEditing) {
   return cabinData;
 }
 
-export async function deleteCabin(id, image = '') {
+export async function deleteCabin(id, image = "") {
   // > #1. Read the cabin data pertinent to this id - to be used reverting the data if failed deleting the image
   const { data: cabin, error: cabinReadError } = await supabase
-    .from('cabins')
-    .select('*')
-    .eq('id', id);
+    .from("cabins")
+    .select("*")
+    .eq("id", id);
   if (cabinReadError) {
     console.error(cabinReadError);
     throw new Error("Can't retrieve cabin information from DB");
   }
   const backupCabinData = cabin[0];
-  const imgFileName = backupCabinData.image.split('/').pop();
+  const imgFileName = backupCabinData.image.split("/").pop();
 
   // > #2. Delete the cabin row from DB cabins table
   const { error: cabinDeleteError } = await supabase
-    .from('cabins')
+    .from("cabins")
     .delete()
-    .eq('id', id);
+    .eq("id", id);
   // GUARD CLAUSE - HANDLE ERROR OBJECT FROM SUPABASE RESPONSE
   if (cabinDeleteError) {
-    console.error('Error deleting cabin:', cabinDeleteError);
-    throw new Error('Cabin could not be deleted');
+    console.error("Error deleting cabin:", cabinDeleteError);
+    throw new Error("Cabin could not be deleted");
   }
 
   // > #3. Check if image is being shared with more than 1 entry
   const { data: imageBearerList, error: imageBearerListError } = await supabase
-    .from('cabins')
-    .select('image')
-    .eq('image', image);
+    .from("cabins")
+    .select("image")
+    .eq("image", image);
   if (imageBearerListError) {
     //  Error handling
     console.error(imageBearerListError);
     throw new Error(
-      'Encountered a problem gathering list of cabins using the same image. Cabin did not get deleted.'
+      "Encountered a problem gathering list of cabins using the same image. Cabin did not get deleted."
     );
   }
   // console.log(imageBearerList);
@@ -169,7 +169,7 @@ export async function deleteCabin(id, image = '') {
   if (!imageBearerList.length) {
     // > #4. Delete Image from DB bucket
     const { error: fileRemoveError } = await supabase.storage
-      .from('cabin-images')
+      .from("cabin-images")
       .remove([imgFileName]);
     // const fileRemoveError = true; // FOR TESTING FILEREMOVE ERROR
     if (fileRemoveError) {
@@ -178,7 +178,7 @@ export async function deleteCabin(id, image = '') {
       //Error handling
       console.error(fileRemoveError);
       throw new Error(
-        'Encountered a problem removing the cabin image. Cabin did not get deleted.'
+        "Encountered a problem removing the cabin image. Cabin did not get deleted."
       );
     }
   }
