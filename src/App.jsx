da52a392c34127763ea8ab321af1491cc76162f1
Erhan Ertem <eertem@gmail.com>
2024-10-20 01:43:59 +0300

    Make Logo, darkmode button and darkmode css toggle

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Checkin from "./pages/Checkin";
 import AuthRoute from "./ui/AuthRoute";
 import AppLayout from "./ui/AppLayout";
 import ProtectedRoute from "./ui/ProtectedRoute";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 // > #1. SETUP TANSTACK QUERY CLIENT W/CACHING SUPPORT
 const queryClient = new QueryClient({
@@ -31,114 +32,116 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    // > #2. PROVIDE TANSTACK QUERY CLIENT
-    <QueryClientProvider client={queryClient}>
-      {/* QUERY DEVTOOLS */}
-      <ReactQueryDevtools
-        initialIsOpen={false}
-        buttonPosition={"bottom-left"}
-        position={"bottom"}
-      />
-      {/* Provide global styled-components */}
+    <DarkModeProvider>
+      {/* // > #2. PROVIDE TANSTACK QUERY CLIENT */}
+      <QueryClientProvider client={queryClient}>
+        {/* QUERY DEVTOOLS */}
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition={"bottom-left"}
+          position={"bottom"}
+        />
+        {/* Provide global styled-components */}
 
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                {/* Applayout wraps all our endpoints. So wrapping Applayout component by ProtectedRoute help us protect the Outlet/children components as well automatically */}
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
-            {/* Redirecting from root to /dashboard */}
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
             <Route
-              index
               element={
-                // Used Navigate component to not dublicate <Dashboard/> component
-                // replace attribute ensures this redirect action replaces the current entry in the history to prevent user from going back
-                // to points to route that handles <Dashboard/> component
-                <Navigate
-                  replace
-                  to="dashboard"
-                />
+                <ProtectedRoute>
+                  {/* Applayout wraps all our endpoints. So wrapping Applayout component by ProtectedRoute help us protect the Outlet/children components as well automatically */}
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              {/* Redirecting from root to /dashboard */}
+              <Route
+                index
+                element={
+                  // Used Navigate component to not dublicate <Dashboard/> component
+                  // replace attribute ensures this redirect action replaces the current entry in the history to prevent user from going back
+                  // to points to route that handles <Dashboard/> component
+                  <Navigate
+                    replace
+                    to="dashboard"
+                  />
+                }
+              />
+              {/* Main routes */}
+              <Route
+                path="dashboard"
+                element={<Dashboard />}
+              />
+              <Route
+                path="bookings"
+                element={<Bookings />}
+              />
+              <Route
+                path="bookings/:bookingId"
+                element={<Booking />}
+              />
+              <Route
+                path="checkin/:bookingId"
+                element={<Checkin />}
+              />
+              <Route
+                path="cabins"
+                element={<Cabins />}
+              />
+              <Route
+                path="settings"
+                element={<Settings />}
+              />
+              <Route
+                path="users"
+                element={<Users />}
+              />
+              <Route
+                path="account"
+                element={<Account />}
+              />
+            </Route>
+
+            {/* THESE ROUTES BELOW DOES NOT NEED TO DISPLAY APPLAYOUT */}
+
+            <Route
+              path="login"
+              element={
+                <AuthRoute>
+                  <Login />
+                </AuthRoute>
               }
             />
-            {/* Main routes */}
-            <Route
-              path="dashboard"
-              element={<Dashboard />}
-            />
-            <Route
-              path="bookings"
-              element={<Bookings />}
-            />
-            <Route
-              path="bookings/:bookingId"
-              element={<Booking />}
-            />
-            <Route
-              path="checkin/:bookingId"
-              element={<Checkin />}
-            />
-            <Route
-              path="cabins"
-              element={<Cabins />}
-            />
-            <Route
-              path="settings"
-              element={<Settings />}
-            />
-            <Route
-              path="users"
-              element={<Users />}
-            />
-            <Route
-              path="account"
-              element={<Account />}
-            />
-          </Route>
 
-          {/* THESE ROUTES BELOW DOES NOT NEED TO DISPLAY APPLAYOUT */}
-
-          <Route
-            path="login"
-            element={
-              <AuthRoute>
-                <Login />
-              </AuthRoute>
-            }
-          />
-
-          {/* Handle undefined routes */}
-          <Route
-            path="*"
-            element={<PageNotFound />}
-          />
-        </Routes>
-      </BrowserRouter>
-      <Toaster
-        position="top-center"
-        gutter={12}
-        containerStyle={{ margin: "8px" }}
-        toastOptions={{
-          success: {
-            duration: 4000,
-          },
-          error: {
-            duration: 3000,
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
-          },
-        }}
-      />
-    </QueryClientProvider>
+            {/* Handle undefined routes */}
+            <Route
+              path="*"
+              element={<PageNotFound />}
+            />
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 4000,
+            },
+            error: {
+              duration: 3000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
 const DarkModeContext = createContext();
@@ -6,6 +6,16 @@ const DarkModeContext = createContext();
 function DarkModeProvider({ children }) {
   // CUSTOM HOOK STATE THTA IS CAPABLE OF REGISTER TO LOCALSTORAGE OF THE BROWSER
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
 
   function toggleDarkMode() {
     setIsDarkMode((dark) => !dark);
