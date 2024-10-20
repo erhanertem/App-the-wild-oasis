d0b33a6c8e674c04fdcf601a7f490ee2a1df48e2
Erhan Ertem <eertem@gmail.com>
2024-10-20 15:34:23 +0300

    Fix darkmode issues

--- src/features/dashboard/DashboardFilter.jsx
+++ src/features/dashboard/DashboardFilter.jsx
@@ -6,8 +6,8 @@ function DashboardFilter() {
       filterField="last"
       options={[
         { value: "7", label: "Last 7 days" },
-        { value: "30", label: "Last 30 days" },
-        { value: "90", label: "Last 90 days" },
+        { value: "30", label: " 30 days" },
+        { value: "90", label: " 90 days" },
       ]}
     />
   );
