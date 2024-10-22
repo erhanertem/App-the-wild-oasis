b791fc4ceb610c3864510fdc744d7d0e47846c36
Erhan Ertem <eertem@gmail.com>
2024-10-23 00:13:53 +0300

    Add note why we use window API instead of RR useNavigate hook

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,9 @@ createRoot(document.getElementById("root")).render(
       // Identify the fallback component when an erro boundary is caught
       FallbackComponent={ErrorFallback}
       // Error boundary reset handler
+      /**
+       * Using window.location.replace("/") causes a full-page reload. This means that the current state of the application is completely cleared, and the browser navigates to the specified URL (/). The entire React app is reloaded, and any potential memory leaks or problematic state that caused the error are fully reset.
+       */
       onReset={() => window.location.replace("/")}
     >
       <StyleSheetManager shouldForwardProp={() => true}>
