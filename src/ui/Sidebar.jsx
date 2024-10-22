4afdd7188bd8bf79ed707a721e522895fd2ea99d
Erhan Ertem <eertem@gmail.com>
2024-10-23 00:23:39 +0300

    Get rid of developer data re-uplader component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import Uploader from "../data/Uploader";
+// import Uploader from "../data/Uploader";
 
 // Parent styling
 const StyledSidebar = styled.aside`
@@ -21,7 +21,7 @@ function Sidebar() {
       <Logo />
       <MainNav />
 
-      <Uploader />
+      {/* <Uploader /> */}
     </StyledSidebar>
   );
 }
